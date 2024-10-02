1d9f0aac61edbe349c7352ba54ea9915eeed4c40
Nick Klonne <nicholasklonne@gmail.com>
2024-10-01 22:11:41 -0500

    Kinda looks like a keyboard, but doesn't do much

--- app/src/main/java/com/nickklonne/dakotakeyboard/DakotaKeyboardInputMethodService.kt
+++ app/src/main/java/com/nickklonne/dakotakeyboard/DakotaKeyboardInputMethodService.kt
@@ -2,13 +2,44 @@ package com.nickklonne.dakotakeyboard
 
 import android.annotation.SuppressLint
 import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.Keyboard
+import android.inputmethodservice.KeyboardView
 import android.view.View
 
 class DakotaKeyboardInputMethodService : InputMethodService() {
 
     @SuppressLint("InflateParams")
     override fun onCreateInputView(): View {
-        val view = layoutInflater.inflate(R.layout.delete_me, null)
+        val view: KeyboardView = layoutInflater.inflate(R.layout.dakota_keyboard_view, null) as KeyboardView
+        val keyboard = Keyboard(this, R.xml.dakota_keyboard_primary)
+        view.keyboard = keyboard
+
+        // TODO: Implement a real listener
+        view.setOnKeyboardActionListener(object : KeyboardView.OnKeyboardActionListener {
+            override fun onPress(primaryCode: Int) {
+            }
+
+            override fun onRelease(primaryCode: Int) {
+            }
+
+            override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
+            }
+
+            override fun onText(text: CharSequence?) {
+            }
+
+            override fun swipeLeft() {
+            }
+
+            override fun swipeRight() {
+            }
+
+            override fun swipeDown() {
+            }
+
+            override fun swipeUp() {
+            }
+        })
         return view
     }
 
